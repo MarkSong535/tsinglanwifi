19276a1e480c931590ec32ef169381215f569e17
Mark Song <m1908944341@gmail.com>
2022-11-11 21:12:10 +0800

    Initial Commit

---
+++ tsinglanwifi/tsinglanwifiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tsinglanwifiApp.swift
+//  tsinglanwifi
+//
+//  Created by marksong on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct tsinglanwifiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
